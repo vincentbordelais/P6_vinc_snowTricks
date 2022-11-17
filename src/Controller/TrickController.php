ca4ba4456e632b557b61fe08078a2ef56820bcae
Vincent <vincent.bordelais.dev@gmail.com>
2022-11-17 18:43:01 +0100

    Pages ListOfTricks and ShowOnTrick

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Trick;
+use App\Repository\TrickRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TrickController extends AbstractController
 {
-    #[Route('/trick', name: 'app_trick')]
-    public function index(): Response
+    #[Route('/', name: 'trick_home')]
+    public function home(): Response
     {
+        return $this->render('trick/home.html.twig');
+    }
+
+    #[Route('/trick', name: 'app_trick')]
+    public function index(TrickRepository $trickRepository): Response
+    {
+        $tricks = $trickRepository->findAll();
         return $this->render('trick/index.html.twig', [
-            'controller_name' => 'TrickController',
+            'tricks' => $tricks,
+        ]);
+    }
+
+    #[Route('/trick/{id}', name: 'trick_showOne')]
+    public function showOne(Trick $trick): Response
+    {
+        return $this->render('trick/showOne.html.twig', [
+            'trick' => $trick,
         ]);
     }
 }
