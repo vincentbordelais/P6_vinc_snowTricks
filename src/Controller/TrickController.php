204998f59e71e2482ccb9c0c4a5efd599b9429cf
Vincent <vincent.bordelais.dev@gmail.com>
2022-11-17 19:43:33 +0100

    Page trick/new

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Trick;
+use App\Form\TrickType;
 use App\Repository\TrickRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,6 +25,27 @@ class TrickController extends AbstractController
         $tricks = $trickRepository->findAll();
         return $this->render('trick/index.html.twig', [
             'tricks' => $tricks,
+        ]);
+    }
+
+    #[Route('/trick/new', name: 'trick_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $trick = new Trick();
+
+        $form = $this->createForm(TrickType::class, $trick);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $trick->setCreatedDate(new \DateTime());
+            $trick->setUpdatedDate(new \DateTime());
+            $em->persist($trick);
+            $em->flush();
+            return $this->redirectToRoute('trick_showOne', ['id' => $trick->getId()]);
+        }
+
+        return $this->renderForm('trick/new.html.twig', [
+            'formTrick' => $form,
         ]);
     }
 
