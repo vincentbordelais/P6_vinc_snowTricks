0205e9dc8dd256eef62c40a1e93b85b1d6f4111d
Vincent <vincent.bordelais.dev@gmail.com>
2023-03-02 14:25:29 +0100

    Test2 Activation inscription

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -31,7 +31,7 @@ class RegistrationController extends AbstractController
             $token = bin2hex(random_bytes(16));
             $user->setConfirmationToken($token);
             $now = new DateTime('now', new DateTimeZone('Europe/Paris'));
-            $expirationDate = $now->add(new DateInterval('PT'. 1 .'M'));
+            $expirationDate = $now->add(new DateInterval('PT'. 2 .'M'));
             $user->setConfirmationTokenExpiresAt($expirationDate);
             $user->setRoles(['ROLE_USER']);
             $entityManager->persist($user);
@@ -42,6 +42,11 @@ class RegistrationController extends AbstractController
                 $user->getEmail(),  // to
                 'Activation de votre compte sur le site SnowTricks', // subject
                 'registration/activationEmail.html.twig',['user' => $user, 'token' => $token]);
+
+            // Afficher le fuseau horaire actuel
+            echo date_default_timezone_get();
+            // Afficher l'heure actuelle
+            echo date('Y-m-d H:i:s');
 
             return $this->redirectToRoute('registration_pre_activation', ['email' => $user->getEmail()]);
         }
@@ -59,46 +64,67 @@ class RegistrationController extends AbstractController
         ]);
     }
 
-    #[Route('/inscription/activation/{token}', name: 'registration_activation')] // lien du mail
-    public function activation(Request $request, UserRepository $userRepository, EntityManagerInterface $entityManager, SendMailService $mailService): Response
+    #[Route('/inscription/activation/{token}', name: 'registration_activation')] // lien du 1ier mail
+    public function activation(string $token, UserRepository $userRepository, EntityManagerInterface $entityManager): Response
     {
-        $token = $request->get('token');
-
-        if (!$token) {
-            throw new NotFoundHttpException();
-        }
-
         $user = $userRepository->findOneBy(['confirmationToken' => $token]);
+
         if (!$user) {
-            throw new NotFoundHttpException();
+            throw new NotFoundHttpException('Cet utilisateur n\'existe pas.');
         }
 
         $now = new DateTime('now', new DateTimeZone('Europe/Paris'));
-        if ($now > $user->getConfirmationTokenExpiresAt()) {
-            // Génère un nouveau token et met à jour la date d'expiration
-            $newToken = bin2hex(random_bytes(16));
-            $user->setConfirmationToken($newToken);
-            $expirationDate = $now->add(new DateInterval('PT'. 1 .'M'));
-            $user->setConfirmationTokenExpiresAt($expirationDate);
-            $entityManager->flush();
-
-            // Envoie un nouveau mail d'activation avec un nouveau token
-            $mailService->send(
-                'no-reply@snowtricks.com',
-                $user->getEmail(),
-                'Activation de votre compte sur SnowTricks',
-                'registration/activationEmail.html.twig', ['user' => $user, 'token' => $newToken]);
-
+        
+        // Le token a expiré
+        if ($user->getConfirmationTokenExpiresAt() < $now) {
             return $this->render('registration/newTokenSent.html.twig', [
                 'user' => $user
             ]);
-        } else {
-            $user->setIsVerified(true);
-            $user->setConfirmationToken(null);
-            $user->setConfirmationTokenExpiresAt(null);
-            $entityManager->flush();
-    
-            return $this->redirectToRoute('security_login');
         }
+
+        // Le token est toujours valide
+        $user->setIsVerified(true);
+        $user->setConfirmationToken(null);
+        $user->setConfirmationTokenExpiresAt(null);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('security_login');
     }
+
+    #[Route('/inscription/reactivation/{email}', name: 'registration_reactivate')] // lien du 2e mail
+    public function reactivation(string $email, UserRepository $userRepository, EntityManagerInterface $entityManager, SendMailService $mailService): Response
+    {
+        $user = $userRepository->findOneBy(['email' => $email]);
+
+        if (!$user) {
+            throw new NotFoundHttpException('Cet utilisateur n\'existe pas.');
+        }
+
+        if ($user->getIsVerified()) {
+            throw new \Exception('Ce compte a déjà été activé.');
+        }
+
+        $now = new DateTime('now', new DateTimeZone('Europe/Paris'));
+        
+        // Le token a expiré
+        if ($user->getConfirmationTokenExpiresAt() < $now) {
+            $token = bin2hex(random_bytes(16)); // génère un nouveau token
+            $user->setConfirmationToken($token);
+            $expirationDate = $now->add(new DateInterval('PT'. 2 .'M'));
+            $user->setConfirmationTokenExpiresAt($expirationDate);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $mailService->send(
+                'no-reply@example.com', // from
+                $user->getEmail(),  // to
+                'Activation de votre compte sur le site SnowTricks', // subject
+                'registration/activationEmail.html.twig',['user' => $user, 'token' => $token]);
+
+            return $this->redirectToRoute('registration_pre_activation', ['email' => $user->getEmail()]);
+        }
+        // Le token est toujours valide
+        return $this->redirectToRoute('registration_pre_activation', ['email' => $user->getEmail()]);
+    }
+    
 }
